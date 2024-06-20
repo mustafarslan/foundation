b02865c3b1c18502b74cd404ef3c365460297e89
Mustafa Arslan <mustafarslan35@gmail.com>
2024-06-20 18:40:33 +0300

    Kafka has been added and also docker compose updated with zookeeper and kafka.

--- core/src/main/java/com/foundation/core/service/GreetService.java
+++ core/src/main/java/com/foundation/core/service/GreetService.java
@@ -4,21 +4,47 @@ import com.foundation.core.dto.GreetingResponseDto;
 import com.foundation.core.entity.Greet;
 import com.foundation.core.repository.GreetRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
 @Profile("dev")
 public class GreetService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
     private final ModelMapper modelMapper;
     private final GreetRepository greetRepository;
 
     public GreetingResponseDto greet() {
         Greet greet = greetRepository.findById(1L);
+        sendMessage("hello World");
         return modelMapper.map(greet, GreetingResponseDto.class);
+    }
+
+    public void sendMessage(String message) {
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send("foundation-greet-topic", message);
+        future.whenComplete((result, ex) -> {
+            if(ex == null) {
+                log.error("Sent message=[{}] with offset=[{}]", message, result.getRecordMetadata().offset());
+            } else {
+                log.error("Unable to send message=[{}] due to : {}", message, ex.getMessage());
+            }
+        });
+    }
+
+    @KafkaListener(topics="foundation-greet-topic", groupId="greet")
+    public void listenGroupGreet(String message){
+        log.info("from topic={}, group={}, message:{}", "foundation-greet-topic", "greet", message);
     }
 }
